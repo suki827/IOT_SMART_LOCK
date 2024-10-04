176e38104ab4c6a9d5bb3fc35d3bb003da619f8c
hugo.huan <hugo.huan@outlook.com>
2024-10-05 11:35:10 +1300

    modify

--- sensors/humiture_describe.py
+++ sensors/humiture_describe.py
@@ -1,7 +1,7 @@
 
 import json
 
-from mqtt import mqtt_client
+from mqtt.mqtt_client import MQTTClient
 
 # MQTT 服务器配置
 MQTT_HOST = "mqtt.thingsboard.cloud"  # 替换为你的 MQTT 服务器
@@ -40,7 +40,7 @@ def on_message(client, userdata, msg):
 
 
 # 初始化 MQTT 客户端
-mqtt_client = mqtt_client.MQTTClient()
+mqtt_client = MQTTClient()
 
 # 绑定回调函数
 mqtt_client.on_connect = on_connect
